8ddf11b19403968943330eda1f7876eb50e76dd0
zin koko <zinkoko5211@gmail.com>
2025-12-04 18:33:28 +0900

    added 4.Structure

---
+++ 4.Structure/structure.js
@@ -0,0 +1,10 @@
+class St {
+    constructor(key, str) {
+        this.key = key;
+        this.str = str;
+    }
+}
+
+st = new St(101, "This is hell.");
+console.log(st.key);
+console.log(st.str);
---
+++ 4.Structure/structure.py
@@ -0,0 +1,9 @@
+class St:
+    def __init__(self, key, str):
+        self.key = key
+        self.str = str
+
+st = St(101, "This is hell.")
+
+print(st.key)
+print(st.str)
