c5d7ae1b1a326a0b64b4ee54d8f276c52a2aa5ca
zin koko <zinkoko5211@gmail.com>
2025-12-04 15:55:55 +0900

    add about-pointer.md

--- 3.Recursion/recursion.c
+++ 3.Recursion/recursion.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+int sqSum(int num);
+
+int main() {
+    int result = sqSum(100);
+    printf("%d\n", result);
+    return 0;
+}
+
 int sqSum(int num) {
     int sum = num * num;
     if (num-1) {
         sum += sqSum(num-1);
     }
     return sum;
-}
-
-int main() {
-    int result = sqSum(100);
-    printf("%d\n", result);
-    return 0;
 }
