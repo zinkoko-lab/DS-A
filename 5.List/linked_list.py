efd82e6744a35b41b0fdae18094c053f308c0fe1
zin koko <zinkoko5211@gmail.com>
2025-12-05 09:51:52 +0900

    added add function to linked_list.py

--- 5.List/linked_list.py
+++ 5.List/linked_list.py
@@ -4,6 +4,7 @@ class Node:
         self.val = val
         self.next = next
 
+
 class LinkedList:
 
     def __init__(self, head=None):
@@ -13,7 +14,7 @@ class LinkedList:
         current = self.head
         while current:
             if current.next:
-                print(current.val, end=' -> ')
+                print(current.val, end=" -> ")
             else:
                 print(current.val)
             current = current.next
@@ -33,16 +34,42 @@ class LinkedList:
 
     def addLast(self, val):
         new_node = Node(val)
-        ptr = self.head
-        while ptr.next:
-            ptr = ptr.next
-        ptr.next = new_node
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
 
-    # def add(self, idex)
+    def add(self, index, val):
+        if self.countNode():
+            if index > 0 and index <= (self.countNode() + 1):
+                new_node = Node(val)
+                current_index = 1
+                current = self.head
+                while current_index < (index - 1):
+                    current = current.next
+                    current_index += 1
+                if current.next:
+                    new_node.next = current.next
+                    current.next = new_node
+                else:
+                    current.next = new_node
+            elif index > 0 and index > (self.countNode() + 1):
+                raise IndexError("index out of range")
+            else:
+                self.addLast(val)
+        else:
+            if index == 1 or index == -1:
+                self.addFirst(val)
+            else:
+                raise IndexError("index out of range")
 
 
 ll = LinkedList()
-ll.addFirst('C')
-ll.addFirst('A')
-ll.addLast('D')
+ll.addFirst("C")
+ll.addFirst("B")
+ll.addFirst("A")
+ll.addLast("E")
 print(ll.countNode())
+ll.printList()
+ll.add(-1, "F")
+ll.printList()
