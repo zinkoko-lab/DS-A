14114474a89b6d51ec9e44d2aec3a54da294a13c
zin koko <zinkoko5211@gmail.com>
2025-12-05 14:53:10 +0900

    added remove function to linked_list.py

--- 5.List/linked_list.py
+++ 5.List/linked_list.py
@@ -10,66 +10,144 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
 
-    def printList(self):
-        current = self.head
-        while current:
-            if current.next:
-                print(current.val, end=" -> ")
-            else:
-                print(current.val)
-            current = current.next
-
     def countNode(self):
-        cnt = 0
-        current = self.head
-        while current:
-            cnt += 1
-            current = current.next
-        return cnt
+        count = 0
+        cur = self.head
+        while cur:
+            cur = cur.next
+            count += 1
+        return count
+
+    def printList(self):
+        if not self.head:
+            print("This is an empty linked-list.")
+            return
+
+        cur = self.head
+        while cur:
+            print(cur.val, end=" -> " if cur.next else "\n")
+            cur = cur.next
 
     def addFirst(self, val):
-        new_node = Node(val)
-        new_node.next = self.head
-        self.head = new_node
+        new = Node(val)
+        new.next = self.head
+        self.head = new
 
     def addLast(self, val):
-        new_node = Node(val)
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
+        if not self.head:
+            self.addFirst(val)
+            return
+
+        new = Node(val)
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = new
 
     def add(self, index, val):
-        if self.countNode():
-            if index > 0 and index <= (self.countNode() + 1):
-                new_node = Node(val)
-                current_index = 1
-                current = self.head
-                while current_index < (index - 1):
-                    current = current.next
-                    current_index += 1
-                if current.next:
-                    new_node.next = current.next
-                    current.next = new_node
-                else:
-                    current.next = new_node
-            elif index > 0 and index > (self.countNode() + 1):
-                raise IndexError("index out of range")
-            else:
-                self.addLast(val)
+
+        size = self.countNode()
+
+        # index > size+1 => out of range
+        if index > size + 1:
+            raise IndexError("index out of range")
+            return
+
+        # index == 0 => invalid index
+        if index == 0:
+            raise IndexError("invalid index number")
+            return
+
+        #  index < 0, 0 < index < size+1 => ok
+        # index == 1 => addFirst(index)
+        if index == 1:
+            self.addFirst(val)
+
+        # index < 0 or index == size + 1 => addLast(index)
+        elif index < 0 or index == size + 1:
+            self.addLast(val)
+
+        # 1 < index <= size + 1
         else:
-            if index == 1 or index == -1:
-                self.addFirst(val)
-            else:
-                raise IndexError("index out of range")
+            new = Node(val)
+            cur = self.head
+            cur_idx = 1
+            while cur_idx < index - 1:
+                cur = cur.next
+                cur_idx += 1
+            new.next = cur.next
+            cur.next = new
+
+    def removeFirst(self):
+        if not self.head:
+            raise IndexError("This is an empty linked list and no element to remove.")
+            return
+
+        cur = self.head
+        self.head = cur.next
+        cur.next = None
+
+    def removeLast(self):
+        if not self.head:
+            raise IndexError("This is an empty linked list and no element to remove.")
+            return
+
+        if self.countNode() == 1:
+            self.removeFirst()
+
+        else:
+            cur = self.head
+            count = 1
+            while count < self.countNode() - 1:
+                cur = cur.next
+                count += 1
+            cur.next = None
+
+    def remove(self, index):
+
+        size = self.countNode()
+
+        # index > size+1 => out of range
+        if index > size + 1:
+            raise IndexError("index out of range")
+            return
+
+        # index == 0 => invalid index
+        if index == 0:
+            raise IndexError("invalid index number")
+            return
+
+        # index == 1 => removeFirst
+        if index == 1:
+            self.removeFirst()
+
+        # index == size or index < 0
+        elif index == size and index < 0:
+            self.removeLast()
+
+        # 1 < index < size
+        else:
+            cur = self.head
+            count = 1
+            while count < index - 1:
+                cur = cur.next
+                count += 1
+            target = cur.next
+            cur.next = target.next
+            target.next = None
 
 
-ll = LinkedList()
-ll.addFirst("C")
-ll.addFirst("B")
-ll.addFirst("A")
-ll.addLast("E")
-print(ll.countNode())
-ll.printList()
-ll.add(-1, "F")
-ll.printList()
+some_quote = LinkedList()
+some_quote.addFirst("if")
+some_quote.addLast("there")
+some_quote.add(3, "is")
+some_quote.add(4, "no")
+some_quote.add(5, "struggle")
+some_quote.add(6, "there")
+some_quote.add(7, "is")
+some_quote.add(8, "no")
+some_quote.add(9, "progress")
+some_quote.printList()
+some_quote.remove(5)
+some_quote.add(5, "apple")
+some_quote.printList()
